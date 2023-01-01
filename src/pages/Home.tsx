84740358d640be2851647b112802924bd86ca165
Beluga-Whale <savaddee159@gmail.com>
2023-01-01 11:00:11 +0700

    add search function to search title or content

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import BlogList from '../components/BlogList';
 import { gql, useQuery } from '@apollo/client';
 
@@ -17,20 +17,41 @@ const GET_BLOGS = gql`
 `;
 
 const Home = () => {
+    const [query, setQuery] = useState('');
     const { loading, error, data } = useQuery(GET_BLOGS);
 
     if (loading) return <div>Loading...</div>;
 
     if (error) return <div>{`Error ${error.message}`}</div>;
 
+    const search = (data: any) => {
+        return data.posts.filter(
+            (post: any) =>
+                post.content.html
+                    .replace(/<[^>]+>/g, '')
+                    .toLowerCase()
+                    .includes(query) || post.title.toLowerCase().includes(query)
+        );
+    };
+
     return (
         <div className="max-w-4xl mx-auto p-4 min-h-screen ">
-            <div>
-                <h1 className="font-black text-3xl md:text-5xl ">Beluga</h1>
-                <p className="md:text-lg">Our blog posts.</p>
-                <hr />
+            <div className="flex justify-between mb-2">
+                <div>
+                    <h1 className="font-black text-3xl md:text-5xl ">Beluga</h1>
+                    <p className="md:text-lg">Our blog posts.</p>
+                </div>
+                <input
+                    type="text"
+                    className="border-2 w-72 h-16 border-teal-600 rounded-md"
+                    value={query}
+                    placeholder="Search content or title"
+                    onChange={e => setQuery(e.target.value)}
+                />
             </div>
-            <BlogList posts={data.posts} />
+            <hr />
+
+            <BlogList posts={search(data)} />
         </div>
     );
 };
